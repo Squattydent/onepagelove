0c29eabad68dc7d7e48517ad6180623e9e3ea9f1
Rob Hope <rob@onepagelove.com>
2016-05-01 10:11:15 +0200

    v6.6.9.1 - Header styles now REM

--- frontend/inc/nav-subscribe.php
+++ frontend/inc/nav-subscribe.php
@@ -1,9 +1,9 @@
 <ul>
-  <li><a href="https://onepagelove.com/go/newsletter" target="_blank">Newsletter</a></li>  
-  <li><a href="http://onepagelove.com/feed" target="_blank">RSS feed</a></li>
-  <li><a href="http://www.producthunt.com/posts/onepagelove " target="_blank">Product Hunt</a></li>  
-  <li><a href="http://twitter.com/OnePageLove" target="_blank">Twitter</a></li>
-  <li><a href="http://www.facebook.com/onepagelove" target="_blank">Facebook</a></li>
-  <li><a href="http://pinterest.com/onepagelove/" target="_blank">Pinterest</a></li>
-  <li><a href="https://plus.google.com/101808922664177427937" target="_blank" rel="publisher">Google+</a></li>  
+	<li><a class="sub-n" href="https://onepagelove.com/go/newsletter" target="_blank">Newsletter</a></li>  
+	<li><a class="sub-r" href="http://onepagelove.com/feed" target="_blank">RSS feed</a></li>
+	<li><a class="sub-h" href="http://www.producthunt.com/posts/onepagelove " target="_blank">Product Hunt</a></li>  
+	<li><a class="sub-t" href="http://twitter.com/OnePageLove" target="_blank">Twitter</a></li>
+	<li><a class="sub-f" href="http://www.facebook.com/onepagelove" target="_blank">Facebook</a></li>
+	<li><a class="sub-p" href="http://pinterest.com/onepagelove/" target="_blank">Pinterest</a></li>
+	<li><a class="sub-g" href="https://plus.google.com/101808922664177427937" target="_blank" rel="publisher">Google+</a></li>  
 </ul>
