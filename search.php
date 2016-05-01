ef55b1ed3254fa277526ec044203fc1e0fd11114
Rob Hope <rob@onepagelove.com>
2016-05-01 18:25:01 +0200

    v6.6.9.7 - Search.php extra search bar

--- search.php
+++ search.php
@@ -1,45 +1,52 @@
 <?php get_header(); ?>
 
-<div class="archive-header">
-	
-		 <div class="archive-header-info">
-	
-			<div class="archive-title"><h1><?php 
+	<div class="archive-header">
 
-        	// search			
-			if (is_search()) {
-			printf( __( '%d %s' ,'onepagelove' ), $wp_query->found_posts, _n( ' search result', 'search results', $wp_query->found_posts), get_search_query() );
-            echo ' for &quot;'.esc_html($s).'&quot;';
-			};
+		<div class="archive-header-info">
 
-			?></h1>
-			</div><!--  /.archive-title -->
-			
-			<div class="archive-description">  
-				<div class="results-search">
-              	<form class="search" action="<?php print get_site_url(); ?>/" method="get"><input type="text" class="search-field" name="s" value="<?php  if (is_search()) {esc_attr_e($s);} else {echo ('Search');} ?>" onFocus="this.value=''" /></form>
-            	</div><!-- /.header-search -->
+			<div class="archive-title">
+
+				<h1>
+
+					<?php 
+					
+						if (is_search()) {
+						printf( __( '%d %s' ,'onepagelove' ), $wp_query->found_posts, _n( ' search result', 'search results', $wp_query->found_posts), get_search_query() );
+						echo ' for &quot;'.esc_html($s).'&quot;';
+						};
+
+					?>
+
+				</h1>
+
 			</div>
- 	
-		 </div><!--  /.archive-header-info -->
-		
+
+			<div class="archive-description">  
+
+				<div class="results-search">
+
+					<form class="search" action="<?php print get_site_url(); ?>/" method="get"><input type="text" class="search-field" name="s" value="<?php  if (is_search()) {esc_attr_e($s);} else {echo ('Search');} ?>" onFocus="this.value=''" /></form>
+
+				</div>
+
+			</div>
+
+		</div><!--  /.archive-header-info -->
+
 		<div class="clear"></div>
-		
-</div><!--  /.archive-header -->
 
-   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-     
+	</div><!--  /.archive-header -->
 
-      <?php get_template_part('loop'); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+		<?php get_template_part('loop'); ?>
 
-      <?php endwhile; else: ?>
-        
-        <div class="single-container"><p>Sorry, no content matched your criteria:(</p><p>Try use different keywords or browse using the categories.</p></div>
+	<?php endwhile; else: ?>
 
-       <?php endif; ?>
+		<div class="single-container"><p>Sorry, no content matched your criteria:(</p><p>Try use different keywords or browse using the categories.</p></div>
 
-	  <?php include ("frontend/inc/pagination.php"); ?>
-        
+	<?php endif; ?>
+
+	<?php include ("frontend/inc/pagination.php"); ?>
 
 <?php get_footer(); ?>
