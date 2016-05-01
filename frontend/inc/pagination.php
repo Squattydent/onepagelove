d49137f2f83b5838e96abbe12398ad3444d709ab
Rob Hope <rob@onepagelove.com>
2016-05-01 13:10:26 +0200

    v6.6.9.5 - Archives.scss cleaned up
    - Cleaned up a few more mediaqueries

--- frontend/inc/pagination.php
+++ frontend/inc/pagination.php
@@ -1,8 +1,11 @@
 <div class="browsing">
+
     <div class="browsing-nav">
+
         <div class="browsing-nav-left"><?php previous_posts_link('Previous') ?>&nbsp;</div>
         <div class="browsing-nav-mid"><div class="navigation"><?php if(function_exists('pagenavi')) { pagenavi(); } ?></div></div>
         <div class="browsing-nav-right"><?php next_posts_link('Next') ?>&nbsp;</div>
-    </div><!-- /.browsing-nav -->
-    <div class="clear"></div>
-</div><!--  /.browsing -->
+        
+    </div>
+
+</div>
--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@
 // -------------------------------------------------------------
 
 // Theme Version
-define( 'OPL_THEME_VERSION' , '6.6.9.4' );
+define( 'OPL_THEME_VERSION' , '6.6.9.5' );
 
 // Feed Links
 add_theme_support( 'automatic-feed-links' );
