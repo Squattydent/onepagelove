53e0deff3722cd5233a4c135a4a2bd4a82c03967
Rob Hope <rob@onepagelove.com>
2016-06-01 10:48:40 +0200

    v6.6.15 - Testimonial styles
    - Fix on testimonial styles in pages and single posts
    - Added more Hustle Panda domains

--- frontend/inc/random-hustle-panda.php
+++ frontend/inc/random-hustle-panda.php
@@ -2,23 +2,38 @@
 
 	$words  = array(
 
-		 // 'feltu.com for $79' 	, // sold
-			'punlet.com for $99' 	, 
-			'fashed.com for $99' 	, 
-			'umbril.com for $149' 	, 
-			'scampish.com for $99' 	, 
-			'skippets.com for $299' , 
-			'rattr.com for $99' 	, 
-			'warked.com for $149' 	, 
-			'camdea.com for $799' 	, 
-			'orcly.com for $199' 	, 
-			'meringa.com for $199' 	, 
-			'runted.com for $99' 	, 		
-			'flumin.com for $99' 	,	 		
-			'picrio.com for $499' 	// no comma
+		'bascinet.com for $499',
+		'buskle.com for	$99',
+		'camdea.com for	$799',
+		'duntle.com for	$99',
+		'eazzo.com for	$199',
+		'flappets.com for	$99',
+		'flumin.com for	$99',
+		'giblr.com for	$299',
+		'hulid.com for	$399',
+		'lattela.com for	$299',
+		'lirate.com for 	$99',
+		'marbula.com	 for $99',
+		'meringa.com for	$199',
+		'orcly.com for 	$199',
+		'picrio.com	 for $499',
+		'punlet.com  for 	$99',
+		'rattr.com  for 	$99',
+		'rinsible.com  for 	$199',
+		'rumalo.com for 	$99',
+		'runted.com  for 	$99',
+		'scampish.com  for 	$99',
+		'skippets.com  for 	$299',
+		'umbril.com for 	$149',
+		'warked.com  for 	$149',
+		'yukut.com	 for $199',
+		'yumow.com for 	$199' // no comma
 
 		);
 
 	echo $words[rand(0, count($words) - 1)];
 
 ?>
+
+
+
--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@
 // -------------------------------------------------------------
 
 // Theme Version
-define( 'OPL_THEME_VERSION' , '6.6.14' );
+define( 'OPL_THEME_VERSION' , '6.6.15' );
 
 // Feed Links
 add_theme_support( 'automatic-feed-links' );
