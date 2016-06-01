012bc38a62deb4a022a44c538c53d39dd00ae779
Rob Hope <rob@onepagelove.com>
2016-06-01 16:00:59 +0200

    v6.6.16 - Reduced Currl Bundle price to $19

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@
 // -------------------------------------------------------------
 
 // Theme Version
-define( 'OPL_THEME_VERSION' , '6.6.15' );
+define( 'OPL_THEME_VERSION' , '6.6.16' );
 
 // Feed Links
 add_theme_support( 'automatic-feed-links' );
