6135acd561f36f7fe156bcbfadd597e763183530
Rob Hope <rob@onepagelove.com>
2016-08-04 15:16:31 +0200

    v6.8.12 - Removed background image on home
    - Also 12th thumb now links to hosting page

--- frontend/inc/loop-bundle.php
+++ frontend/inc/loop-bundle.php
@@ -2,7 +2,7 @@
 	
 	<div class="thumb-image">
 
-		<a href="https://www.bluehost.com/track/onepagelove/julythumb" title="Host One Page websites for only $3.49/mo">
+		<a href="https://onepagelove.com/hosting" title="Host One Page websites for only $3.49/mo">
 		<img src="https://onepagelove.com/wp-content/uploads/2016/06/thumbnail-july.jpg" alt="july-thumb" width="400" height="211" class="alignnone size-full" /></a>
 
 	</div>
@@ -13,13 +13,13 @@
 
 			<div class="thumb-name">
 
-				<h2><a href="https://www.bluehost.com/track/onepagelove/julythumb" title="Host One Page websites for only $3.49/mo">Host One Page websites for only $3.49/mo</a></h2>
+				<h2><a href="https://onepagelove.com/hosting" title="Host One Page websites for only $3.49/mo">Host One Page websites for only $3.49/mo</a></h2>
 
 			</div>
 
 			<div class="thumb-cat">
 
-				<a href="https://www.bluehost.com/track/onepagelove/julythumb"><?php echo date("F"); ?> Promotion (Save 50%)</a>
+				<a href="https://onepagelove.com/hosting"><?php echo date("F"); ?> Promotion (Save 50%)</a>
 
 			</div>
 
