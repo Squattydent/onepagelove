fd204c87153fcf2d1c8a1c985d308c3636260115
Rob Hope <rob@onepagelove.com>
2016-10-29 13:24:29 +0200

    v6.8.23 - Fixed Page Section Title Bug

--- frontend/inc/loop-hosting.php
+++ frontend/inc/loop-hosting.php
@@ -2,8 +2,8 @@
 	
 	<div class="thumb-image">
 
-		<a href="https://onepagelove.com/hosting" title="Host One Page websites for only $3.49/mo">
-		<img src="https://onepagelove.com/wp-content/uploads/2016/09/thumb-hosting-retina.jpg" alt="july-thumb" width="400" height="211" class="alignnone size-full" /></a>
+		<a href="https://onepagelove.com/go/bluehost" title="Host One Page websites for only $2.95/mo">
+		<img src="https://onepagelove.com/wp-content/uploads/2016/06/thumbnail-july.jpg" alt="Exclusive Hosting Special" width="400" height="211" class="alignnone size-full" /></a>
 
 	</div>
 
@@ -13,13 +13,13 @@
 
 			<div class="thumb-name">
 
-				<h2><a href="https://onepagelove.com/hosting" title="Host One Page websites for only $3.49/mo">Host One Page websites for only $3.49/mo</a></h2>
+				<h2><a href="https://onepagelove.com/go/bluehost" title="Host One Page websites for only $3.49/mo">Host One Page websites for only $2.95/mo</a></h2>
 
 			</div>
 
 			<div class="thumb-cat">
 
-				<a href="https://onepagelove.com/hosting"><?php echo date("F"); ?> Promotion (Save 50%)</a>
+				<a href="https://onepagelove.com/go/bluehost">Exclusive <?php echo date("F"); ?> only promotion (Save 63%)</a>
 
 			</div>
 
--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@
 // -------------------------------------------------------------
 
 // Theme Version
-define( 'OPL_THEME_VERSION' , '6.8.21' );
+define( 'OPL_THEME_VERSION' , '6.8.23' );
 
 // Feed Links
 add_theme_support( 'automatic-feed-links' );
