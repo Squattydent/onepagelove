963d672df05c8e7787ff117f7fc46c7b280e4327
Rob Hope <rob@onepagelove.com>
2017-08-14 16:54:41 +0200

    v6.10.8 - Fixed Header Bar

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.10.7
+ * @version 6.10.8
  *
 */ 
 
@@ -22,7 +22,7 @@
 // -------------------------------------------------------------
 
 // Theme Version
-define( 'OPL_THEME_VERSION' , '6.10.7' );
+define( 'OPL_THEME_VERSION' , '6.10.8' );
 
 // Content Width
 global $content_width;
