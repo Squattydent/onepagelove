1b2591d8064833f9e7f3910201be8fd7c8e1819c
Rob Hope <rob@onepagelove.com>
2017-09-13 08:17:03 +0200

    v6.11.5 - Re-added RSS feed customization
    - Now includes promo image above review

--- backend/functions-rss.php
+++ backend/functions-rss.php
@@ -1,27 +1,28 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.1
+ * @version 6.11.5
  *
 */ 
 
 // -------------------------------------------------------------
-// RSS Feed Edits
+// Add Promo Image above the content in RSS Feed
 // -------------------------------------------------------------
 
-// Add Promo Image above the content in RSS Feed
-function fields_in_feed($content) {
+function onepagelove_custom_rss_feed($content) {
 
-    if(is_feed()) {
+    if ( is_feed() ) {
 
         $post_id = get_the_ID();
-	    $output = '<p><img src="';
+	    $output  = '<p><img src="';
 	    $output .= '' . get_post_meta($post_id, 'promo_image', true) . '';
 	    $output .= '" /></p>';   
 	    $content = $output.$content;
-		return $content;
-
+		
     }
 
+    return $content;
+
 }
-add_filter('the_content','fields_in_feed');
+
+add_filter('the_content','onepagelove_custom_rss_feed');
