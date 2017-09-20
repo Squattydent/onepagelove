89394813a0bb54969d95b947e429280196e68b00
Rob Hope <rob@onepagelove.com>
2017-09-20 16:07:23 +0200

    v6.11.15 - Improvements to RSS Feed
    - Added direct links and link to review

--- backend/functions-rss.php
+++ backend/functions-rss.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.5
+ * @version 6.11.15
  *
 */ 
 
@@ -13,12 +13,23 @@ function onepagelove_custom_rss_feed($content) {
 
     if ( is_feed() ) {
 
-        $post_id = get_the_ID();
-	    $output  = '<p><img src="';
-	    $output .= '' . get_post_meta($post_id, 'promo_image', true) . '';
-	    $output .= '" /></p>';   
-	    $content = $output.$content;
-		
+    	if ( !in_category((array("Blog","Journal","Articles","Resources","Interviews","Round Ups","Sponsored"))) ) {
+
+	        $post_id  		= get_the_ID();
+	        $post_image 	= get_post_meta($post_id, 'promo_image', true);
+	        $post_link		= get_the_permalink();
+	        $direct_link	= get_post_meta($post_id, 'site_url', true);
+		    $output  		= '<p><a href="' . $post_link . '"><img src="' . $post_image . '" /></a></p>'; 
+		    $meta 			= '<p><a href="' . $post_link . '">Full Review</a> | <a href="' . $direct_link . '">Direct Link</a></p>';  
+		    $content 		= $output.$content.$meta;
+
+    	}
+
+    	else {
+
+    		 $content 		= $content;
+    	};
+
     }
 
     return $content;
