fc93ca79244ce35c7a4a58976ab55b4365dd0ef3
Rob Hope <rob@onepagelove.com>
2017-09-28 17:41:49 +0200

    v6.11.29 - Added extra item to Info dropdown nav

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.28
+ * @version 6.11.29
  *
 */ 
 
@@ -9,7 +9,7 @@
 // Theme Defining
 // -------------------------------------------------------------
 
-define( 'OPL_THEME_VERSION' , '6.11.28' );                                                      # Theme Version
+define( 'OPL_THEME_VERSION' , '6.11.29' );                                                      # Theme Version
 global $content_width; if ( ! isset( $content_width ) ) $content_width = 1600;                  # Content Width
 
 # ------------------------------------------------------------------------
--- template-parts/navs/info.php
+++ template-parts/navs/info.php
@@ -1,16 +1,17 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.28
+ * @version 6.11.29
  *
 */ 
 ?>
 <ul>
     <li><a href="<?php print get_home_url(); ?>/about">About</a></li>      
-    <li><a href="<?php print get_home_url(); ?>/about#section-advertise">Advertise</a></li>   
-    <li><a href="<?php print get_home_url(); ?>/about#section-roadmap">Milestones</a></li>    
+    <li><a href="<?php print get_home_url(); ?>/about#section-advertise" class="smoothScroll">Advertise</a></li>   
+    <li><a href="<?php print get_home_url(); ?>/about#section-roadmap" class="smoothScroll">Milestones</a></li>    
     <li><a href="<?php print get_home_url(); ?>/blog/journl">OPL Journal</a></li>     
     <li><a href="<?php print get_home_url(); ?>/award-ribbons">Ribbons</a></li> 
     <li><a href="<?php print get_home_url(); ?>/award-certificates">Certificates</a></li> 
-    <li><a href="<?php print get_home_url(); ?>/submit">Submit</a></li>                
+    <li><a href="<?php print get_home_url(); ?>/submit">Submit</a></li> 
+    <li><a href="#footer-container" class="smoothScroll">Newsletter</a></li>                
 </ul>
