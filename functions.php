271c98bad1f36b5f08b3350c90a11983a9d556dd
Rob Hope <rob@onepagelove.com>
2017-10-06 14:43:43 +0200

    v6.11.40 - Improved blog left floating images

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.39
+ * @version 6.11.40
  *
 */ 
 
@@ -9,7 +9,7 @@
 // Theme Defining
 // -------------------------------------------------------------
 
-define( 'OPL_THEME_VERSION' , '6.11.39' );                                                      # Theme Version
+define( 'OPL_THEME_VERSION' , '6.11.40' );                                                      # Theme Version
 global $content_width; if ( ! isset( $content_width ) ) $content_width = 1600;                  # Content Width
 
 # ------------------------------------------------------------------------
