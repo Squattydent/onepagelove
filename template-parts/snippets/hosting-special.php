bbf20a0c7bb41d01cb4a598f2821c0271210c557
Rob Hope <rob@onepagelove.com>
2017-11-24 09:22:06 +0200

    v6.11.55 - Black Friday Price Update

--- template-parts/snippets/hosting-special.php
+++ template-parts/snippets/hosting-special.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.54
+ * @version 6.11.55
  *
 */ 
 ?>
 <!-- See our <a href="https://onepagelove.com/go/bluehost"><?php echo date("F"); ?> $2.95 Hosting Special</a> 🎉 -->
-See our <a href="https://onepagelove.com/go/black-friday">Black Friday $2.95 Hosting Special 🎈</a>
+See our <a href="https://onepagelove.com/go/black-friday">Black Friday $2.65 Hosting Special 🎈</a>
--- template-parts/thumbs/hosting.php
+++ template-parts/thumbs/hosting.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.54
+ * @version 6.11.55
  *
 */ 
 ?>
 <div class="thumb-image">
 
-	<a href="https://onepagelove.com/go/black-friday" title="Host One Page websites for only $2.95/month">
-	<img src="<?php echo get_template_directory_uri(); ?>/img/hustle/hosting/bf.jpg" alt="Exclusive Hosting Special" width="400" height="211" /></a>
+	<a href="https://onepagelove.com/go/black-friday" title="Host One Page websites for only $2.65/month">
+	<img src="<?php echo get_template_directory_uri(); ?>/img/hustle/hosting/bf2017.jpg" alt="Black Friday Hosting Special" width="400" height="211" /></a>
 
 </div>
 
@@ -18,7 +18,7 @@
 
 		<div class="thumb-name">
 
-			<h2><a href="https://onepagelove.com/go/black-friday" title="Host One Page websites for only $2.95/month">Black Friday $2.95 Hosting Special 🎈</a></h2>
+			<h2><a href="https://onepagelove.com/go/black-friday" title="Host One Page websites for only $2.65/month">Black Friday $2.65 Hosting Special 🎈</a></h2>
 			<!-- <h2><a href="https://onepagelove.com/go/bluehost" title="Host One Page websites for only $2.95/month">Host websites for only $2.95/month 🎉</a></h2> -->
 
 		</div>
