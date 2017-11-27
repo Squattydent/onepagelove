4c9911ee7666a27dc3d68d600a6645bc9bfd2b63
Rob Hope <rob@onepagelove.com>
2017-11-27 09:11:26 +0200

    v6.11.57 - Cyber Monday deal

--- template-parts/snippets/hosting-special.php
+++ template-parts/snippets/hosting-special.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.55
+ * @version 6.11.57
  *
 */ 
 ?>
 <!-- See our <a href="https://onepagelove.com/go/bluehost"><?php echo date("F"); ?> $2.95 Hosting Special</a> 🎉 -->
-See our <a href="https://onepagelove.com/go/black-friday">Black Friday $2.65 Hosting Special 🎈</a>
+See our <a href="https://onepagelove.com/go/cyber-monday">Cyber Monday $2.65 Hosting Special 🎈</a>
--- template-parts/thumbs/hosting.php
+++ template-parts/thumbs/hosting.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.55
+ * @version 6.11.57
  *
 */ 
 ?>
 <div class="thumb-image">
 
-	<a href="https://onepagelove.com/go/black-friday" title="Host One Page websites for only $2.65/month">
-	<img src="<?php echo get_template_directory_uri(); ?>/img/hustle/hosting/bf2017.jpg" alt="Black Friday Hosting Special" width="400" height="211" /></a>
+	<a href="https://onepagelove.com/go/cyber-monday" title="Host One Page websites for only $2.65/month">
+	<img src="<?php echo get_template_directory_uri(); ?>/img/hustle/hosting/cm2017.jpg" alt="Cyber Monday Hosting Special" width="400" height="211" /></a>
 
 </div>
 
@@ -18,14 +18,14 @@
 
 		<div class="thumb-name">
 
-			<h2><a href="https://onepagelove.com/go/black-friday" title="Host One Page websites for only $2.65/month">Black Friday $2.65 Hosting Special 🎈</a></h2>
+			<h2><a href="https://onepagelove.com/go/cyber-monday" title="Host One Page websites for only $2.65/month">Cyber Monday $2.65 Hosting Special 🎈</a></h2>
 			<!-- <h2><a href="https://onepagelove.com/go/bluehost" title="Host One Page websites for only $2.95/month">Host websites for only $2.95/month 🎉</a></h2> -->
 
 		</div>
 
 		<div class="thumb-cat">
 
-			<a href="https://onepagelove.com/go/black-friday">Exclusive Black Friday Weekend Promotion (Save 63%)</a>
+			<a href="https://onepagelove.com/go/cyber-monday">Exclusive Cyber Monday Promotion (Save 63%)</a>
 			<!-- <a href="https://onepagelove.com/go/bluehost">Exclusive <?php echo date("F"); ?> promotion (Save 63%)</a> -->
 
 		</div>
