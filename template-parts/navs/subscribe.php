464f3b5c6af2db9ec22c6e3d14e871ab5e7b1582
Rob Hope <rob@onepagelove.com>
2017-12-12 06:04:05 +0200

    v6.11.63 - Instagram added to Subscribe Nav

--- template-parts/navs/subscribe.php
+++ template-parts/navs/subscribe.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.34
+ * @version 6.11.63
  *
 */ 
 ?>
@@ -9,9 +9,10 @@
 	<li><a class="sub-n smoothScroll" href="#footer-container">Newsletter</a></li>  
 	<li><a class="sub-t" href="https://twitter.com/OnePageLove" target="_blank">Twitter</a></li>
 	<li><a class="sub-y" href="https://www.youtube.com/channel/UCZG5er2inmZntGb4X_jx6Wg" target="_blank">YouTube</a></li>
+	<li><a class="sub-i" href="<?php print get_home_url(); ?>/go/instagram" target="_blank">Instagram</a></li>
 	<li><a class="sub-f" href="https://www.facebook.com/onepagelove" target="_blank">Facebook</a></li>
 	<li><a class="sub-p" href="https://pinterest.com/onepagelove/" target="_blank">Pinterest</a></li>
 	<li><a class="sub-r" href="<?php print get_home_url(); ?>/feed" target="_blank">RSS feed</a></li>
-	<li><a class="sub-e" href="https://onepagelove.com/go/feedly" target="_blank">Feedly</a></li>
+	<li><a class="sub-e" href="<?php print get_home_url(); ?>/go/feedly" target="_blank">Feedly</a></li>
 	<li><a class="sub-g" href="https://plus.google.com/101808922664177427937" rel="publisher">Google+</a></li>
 </ul>
