341a5a019060ed7a56656bbf311725e9b18b7e3b
Rob Hope <rob@onepagelove.com>
2018-01-02 17:30:50 +0200

    v6.11.72 - New YouTube link

--- template-parts/navs/subscribe.php
+++ template-parts/navs/subscribe.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package onepagelove
- * @version 6.11.63
+ * @version 6.11.72
  *
 */ 
 ?>
 <ul>
 	<li><a class="sub-n smoothScroll" href="#footer-container">Newsletter</a></li>  
 	<li><a class="sub-t" href="https://twitter.com/OnePageLove" target="_blank">Twitter</a></li>
-	<li><a class="sub-y" href="https://www.youtube.com/channel/UCZG5er2inmZntGb4X_jx6Wg" target="_blank">YouTube</a></li>
+	<li><a class="sub-y" href="https://www.youtube.com/c/robhope" target="_blank">YouTube</a></li>
 	<li><a class="sub-i" href="<?php print get_home_url(); ?>/go/instagram" target="_blank">Instagram</a></li>
 	<li><a class="sub-f" href="https://www.facebook.com/onepagelove" target="_blank">Facebook</a></li>
 	<li><a class="sub-p" href="https://pinterest.com/onepagelove/" target="_blank">Pinterest</a></li>
